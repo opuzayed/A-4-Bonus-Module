80ba8d54e47b685f82ae7ca1066e10d7514bd8fb
opuzayed <zayedopu4933@gmail.com>
2022-08-13 18:18:02 +0600

    factorial using recurssion

--- a4BonusModule.js
+++ a4BonusModule.js
@@ -9,9 +9,20 @@
 const sum = add(5);
 console.log(sum); */
 //----------------factorial------------
-let factorial = 1;
+/* let factorial = 1;
 for(let i=5; i>=1; i--)
 {
     factorial = factorial * i;
 }
-console.log(factorial);
+console.log(factorial); */
+//----------factorial using recurssion----------
+function factorial(i)
+{
+    if(i == 1)
+    {
+        return 1;
+    }
+    return i * factorial(i-1);
+}
+let result = factorial(5);
+console.log(result);
